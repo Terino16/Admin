7a74e71ec2cf5a1571e61eeb81814db3aedddd30
Terino16 <123401184+Terino16@users.noreply.github.com>
2024-06-17 17:18:02 +0530

    added approve logic and category checked logic

--- components/Business.jsx
+++ components/Business.jsx
@@ -1,10 +1,8 @@
 'use client';
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import Head from 'next/head';
 import BusinessTable from './table';
-import { poppins700, poppins400, poppins500, poppins600 } from '../app/fonts';
-import Sidebar from "./Sidebar";
+import { poppins700} from '../app/fonts';
 import { toast } from "react-toastify";
 
 export default function Business() {
